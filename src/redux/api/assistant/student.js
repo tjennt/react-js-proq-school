6d8c9162d79525271ebfd7565e100eb9c3dbf8a9
the linh <chaulinh0302cr7@gmail.com>
2020-10-10 16:38:59 +0700

    api

--- src/redux/api/assistant/student.js
+++ src/redux/api/assistant/student.js
@@ -1,7 +1,9 @@
-import { API_ENDPOINT } from "../../constants/index";
+import { API_ENDPOINT1 } from "../../constants/index";
 import axiosService from "../../../utility/axiosService/axiosService";
 const url = "students";
 
 export const getDataStudentApi = () => {
-  return axiosService.get(`${API_ENDPOINT}/${url}`);
+  return axiosService.get(
+    `https://mockapi.io/clone/5f817fec5b1f3f00161a6a92/student`
+  );
 };
