467934b31530c70318c5727ba0ef688c15ff616a
neo_binh <neo.nguyen88@gmail.com>
2020-10-13 09:59:42 +0700

    merge

--- src/redux/api/assistant/student.js
+++ src/redux/api/assistant/student.js
@@ -3,11 +3,5 @@ import axiosService from "../../../utility/axiosService/axiosService";
 const url = "students";
 
 export const getDataStudentApi = () => {
-<<<<<<< HEAD
-  return axiosService.get(
-    `https://mockapi.io/projects/5f7ec86c094b670016b76782`
-  );
-=======
   return axiosService.get(`${API_ENDPOINT}/${url}`);
->>>>>>> 3dc3dba5c85002bdd577b4d310dfa30452660c81
 };
