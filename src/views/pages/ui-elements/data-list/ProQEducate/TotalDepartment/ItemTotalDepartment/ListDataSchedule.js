1739c1ef477412b54f3146146c7128ae94d8e066
the linh <chaulinh0302cr7@gmail.com>
2020-10-16 00:07:22 +0700

    modal select teacher

--- src/views/pages/ui-elements/data-list/ProQEducate/TotalDepartment/ItemTotalDepartment/ListDataSchedule.js
+++ src/views/pages/ui-elements/data-list/ProQEducate/TotalDepartment/ItemTotalDepartment/ListDataSchedule.js
@@ -5,13 +5,26 @@ import { getDataClass } from "../../../../../../../redux/actions/dataListAssista
 import { connect } from "react-redux";
 import Moment from "react-moment";
 import { Edit } from "react-feather";
+import { Modal } from "antd";
+import Select from "react-select";
+
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 const ActionsComponent = (props) => {
+  const showModal = () => {
+    props.showModal();
+  };
   return (
     <div className="data-list-action">
-      <Edit className="cursor-pointer mr-1" size={20} />
+      <Edit onClick={showModal} className="cursor-pointer mr-1" size={20} />
     </div>
   );
 };
+const options = [
+  { value: 1, label: "Nguyễn Văn A" },
+  { value: 2, label: "Nguyễn Văn B" },
+  { value: 3, label: "Nguyễn Văn C" },
+  { value: 4, label: "Nguyễn Văn D" },
+];
 class ListDataSchedule extends React.Component {
   static getDerivedStateFromProps(props, state) {
     if (props.dataList.dataschedules !== state.data.length) {
@@ -57,6 +70,7 @@ class ListDataSchedule extends React.Component {
         cell: (row) => (
           <ActionsComponent
             row={row}
+            showModal={this.showModal}
             getData={this.props.getData}
             parsedFilter={this.props.parsedFilter}
             currentData={this.handleCurrentData}
@@ -73,23 +87,68 @@ class ListDataSchedule extends React.Component {
     currentData: null,
     selected: [],
     totalRecords: 0,
+    selectTeacher: 1,
     sortIndex: [],
     addNew: "",
   };
   componentDidMount() {
     this.props.getDataClass();
   }
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = (e) => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = (e) => {
+    this.setState({
+      visible: false,
+    });
+  };
+  handleSelectedOptionChange = (value) => {
+    this.setState({
+      ...this.state,
+      selectTeacher: value,
+    });
+  };
   render() {
     const { data, columns } = this.state;
     return (
-      <Card>
-        <CardHeader>
-          <CardTitle className="font-large-1 text-primary">Lịch học</CardTitle>
-        </CardHeader>
-        <CardBody>
-          <DataTable data={data} columns={columns} noHeader pagination />
-        </CardBody>
-      </Card>
+      <div>
+        <Modal
+          title="Thay đổi giảng viên"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Select
+            style={{ width: "100%" }}
+            id="selectTeacher"
+            value={this.state.selectTeacher}
+            isClearable={true}
+            onChange={this.handleSelectedOptionChange}
+            placeholder="Vui lòng chọn giáo viên thay đổi"
+            // isdisabled={disabled}
+            options={options}
+          ></Select>
+        </Modal>
+        <Card>
+          <CardHeader>
+            <CardTitle className="font-large-1 text-primary">
+              Lịch học
+            </CardTitle>
+          </CardHeader>
+          <CardBody>
+            <DataTable data={data} columns={columns} noHeader pagination />
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 }
