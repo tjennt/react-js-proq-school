9f6d5c703a9e406aca8fe4d0ce2a7071e8b0f63d
neo_binh <neo.nguyen88@gmail.com>
2020-10-29 23:10:53 +0700

    test git-Neo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Router from "./Router";
 import "./components/@vuexy/rippleButton/RippleButton";
+import "./views/FontawsomeIcons";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
-import "./views/FontawsomeIcons";
 const App = (props) => {
   return <Router />;
 };
