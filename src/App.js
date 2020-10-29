598f33b62cb27da0629f936f9e4f8fa7d8f75670
neo_binh <neo.nguyen88@gmail.com>
2020-10-30 00:06:12 +0700

    test 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Router from "./Router";
-import "./components/@vuexy/rippleButton/RippleButton";
+import Router from "./Router";//test
 import "./views/FontawsomeIcons";
+import "./components/@vuexy/rippleButton/RippleButton";
 import "prismjs/themes/prism-tomorrow.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 const App = (props) => {
 
   return <Router />;
-  
+
 };
 
 export default App;
